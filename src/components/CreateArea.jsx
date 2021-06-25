73e1ca17e9eea3d6701514f2aa5b6f8b8f8b4e2e
sarah monica <sarah_boss89@yahoo.no>
2021-06-24 18:55:45 -0700

    first commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  return (
+    <div>
+      <form>
+        <input name="title" value={note.title} placeholder="Title" />
+        <textarea
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
